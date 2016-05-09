3835c6b040f465bf5ac36913c4777644f336c646
Ngô Sỹ Trường <truongfatty94@gmai.com>
2016-05-09 12:06:51 +0700

    first commit

--- FruitUITable/TableViewController.h
+++ FruitUITable/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  FruitUITable
 //
 //  Created by Ngô Sỹ Trường on 5/9/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
